0a88d0af91cd539f1c75ee94b7eaddbb4874f5a8
Davi Grah <davi_laurindo@hotmail.com>
2022-07-23 18:32:20 -0300

    tooltip 3

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-// const projects = document.querySelector('.projects-icon ')
+// TOOLTIP ON MOUSE OVER
 const tooltip1 = document.querySelector('.tooltiptext-1')
 const tooltip2 = document.querySelector('.tooltiptext-2')
 const tooltip3 = document.querySelector('.tooltiptext-3')
@@ -12,43 +12,64 @@ const project4 = document.querySelector('.project-icon-4')
 const project5 = document.querySelector('.project-icon-5')
 const project6 = document.querySelector('.project-icon-6')
 
-// for (let i = 0; i < projects.length; i++) {
-//   projects[i].addEventListener('mouseover', hoverOn)
-// //   projects[i].addEventListener('mouseout', hoverOff)
+project1.addEventListener('mouseover', hoverOn1)
+project2.addEventListener('mouseover', hoverOn2)
+project3.addEventListener('mouseover', hoverOn3)
+project4.addEventListener('mouseover', hoverOn4)
+project5.addEventListener('mouseover', hoverOn5)
+project6.addEventListener('mouseover', hoverOn6)
+
+// project1.addEventListener('mouseout', mouseout1)
+// project2.addEventListener('mouseout', mouseout2)
+// project3.addEventListener('mouseout', mouseout3)
+// project4.addEventListener('mouseout', mouseout4)
+// project5.addEventListener('mouseout', mouseout5)
+// project6.addEventListener('mouseout', mouseout6)
+
+// function hoverOn1 () {
+//   tooltip1.style.opacity = '1'
 // }
 
-project1.addEventListener('mouseover', hoverOn1);
-project2.addEventListener('mouseover', hoverOn2);
-project3.addEventListener('mouseover', hoverOn3);
-project4.addEventListener('mouseover', hoverOn4);
-project5.addEventListener('mouseover', hoverOn5);
-project6.addEventListener('mouseover', hoverOn6);
-
-function hoverOn1 () {
-  tooltip1.style.visibility = 'hidden'
-}
-
-function hoverOn2 () {
-  tooltip2.style.visibility = 'hidden'
-}
-
-function hoverOn3 () {
-  tooltip3.style.visibility = 'hidden'
-}
-
-function hoverOn4 () {
-  tooltip4.style.visibility = 'hidden'
-}
-
-function hoverOn5 () {
-  tooltip5.style.visibility = 'hidden'
-}
-
-function hoverOn6 () {
-  tooltip6.style.visibility = 'hidden'
-}
-// function hoverOff () {
-//   tooltip.classList.remove('tooltiptextvisible')
+// function hoverOn2 () {
+//   tooltip2.style.opacity = '1'
 // }
 
+// function hoverOn3 () {
+//   tooltip3.style.opacity = '1'
+// }
 
+// function hoverOn4 () {
+//   tooltip4.style.opacity = '1'
+// }
+
+// function hoverOn5 () {
+//   tooltip5.style.opacity = '1'
+// }
+
+// function hoverOn6 () {
+//   tooltip6.style.opacity = '1'
+// }
+
+// function mouseout1 () {
+//   tooltip1.style.opacity = '0'
+// }
+
+// function mouseout2 () {
+//   tooltip2.style.opacity = '0'
+// }
+
+// function mouseout3 () {
+//   tooltip3.style.opacity = '0'
+// }
+
+// function mouseout4 () {
+//   tooltip4.style.opacity = '0'
+// }
+
+// function mouseout5 () {
+//   tooltip5.style.opacity = '0'
+// }
+
+// function mouseout6 () {
+//   tooltip6.style.opacity = '0'
+// }
